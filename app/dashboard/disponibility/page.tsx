c7cfeb11b85e83e79ea69d97b9d4518517e871aa
Mathis <mathis.micheau14@gmail.com>
2024-11-21 14:33:10 +0100

    dashboard

---
+++ app/dashboard/disponibility/page.tsx
@@ -0,0 +1,8 @@
+export default function DisponibilityPage() {
+  return (
+    <div>
+      <h1>Disponibility</h1>
+      <p>Disponibility page content</p>
+    </div>
+  );
+}
---
+++ app/dashboard/interveners/page.tsx
@@ -0,0 +1,8 @@
+export default function IntervenerPage() {
+  return (
+    <div>
+      <h1>Intervener</h1>
+      <p>Intervener page content</p>
+    </div>
+  );
+}
